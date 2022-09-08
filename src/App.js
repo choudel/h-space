20eb83cf07ff72b3f0d5f6ee2d1573e4952b5a3f
choudel <chouaibh@gmail.com>
2022-09-08 09:05:03 +0200

    react props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
+import React from 'react';
 import './App.css';
-
+const List = props =>
+  props.list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
 const App = () => {
-const handleChange=event=>{
-  console.log(event.target.value)
-}
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  const handleSearch=event=>{
+    console.log(event.target.value)
+  }
   return (
     <div>
       <h1>My Hacker Stories</h1>
+      <Search onSearch={handleSearch}/>
+      <hr />
+      <List list={stories} />
+    </div>
+  )
+}
+const Search = props => {
+  const [searchTerm, setSearchTerm] = React.useState('')
+  const handleChange = event => {
+    setSearchTerm(event.target.value)
+    props.onSearch(event)
+  }
+  return (
+    <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
-      <hr />
-
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
     </div>
-
-  );
+  )
 }
-
 export default App;
